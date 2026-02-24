a19d74679f0193c2f01571269bb0b75c4f2fd473
vb4point2 <dreamisbest12346754@gmail.com>
2026-02-24 12:47:33 -0800

    Fixed this (TODO - Fix more things)

---
+++ src/Shop.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function Shop() {
+    return (
+        <>
+            <div className="header">
+                <a href="App.tsx"><h1 className='title'>Renton Octonauts</h1></a>
+                <a href="#" className='shop'><h2>Shop</h2></a>
+                <a href="#"><h2 className='cart'>Cart</h2></a>
+            </div>
+            <div>
+                <p>H</p>
+            </div>
+        </>
+    )
+}
+
+export default Shop
